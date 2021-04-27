095f88107c2c0fa1485d539153ae8b9c489cc4a1
dongtian <xiaodong.liu@wealedger.com>
2021-04-27 16:41:03 +0800

    init repostitory

---
+++ oppnys-template-vue2/template/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">vue2 template</div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style scoped>
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
