5698a051198ebea2a5b8c6a6c444519a46b28967
dongtianllll <liuxiaodong@nstc.com.cn>
2024-07-18 23:23:50 +0800

    add vue3 template

--- oppnys-template-vue3-component/template/src/App.vue
+++ oppnys-template-vue3-component/template/src/App.vue
@@ -5,6 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
+    <h2><%= className %></h2>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
